f9708b3931a0fadfea9be9b10404b02b558fb2ef
minMin <goals1005@naver.com>
2022-04-06 15:16:04 +0900

    검증2 - Bean Validation (Form 전송객체 분리): V4를 실무에서 많이 사용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요.")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+//    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
 
     /**
@@ -21,15 +21,15 @@ public class Item {
      * @Range(min = 1000, max = 1000000) : 범위 안의 값이어야 한다.
      * @Max(9999) : 최대 9999까지만 허용한다.
      */
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
